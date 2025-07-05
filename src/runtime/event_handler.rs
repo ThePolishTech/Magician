5386bed1f3a6d7c86af56363c606818a52d8c185
ThePolishTech_LP <162223303+ThePolishTech@users.noreply.github.com>
2025-07-05 12:34:49 +0200

    Magician now sends wakeup message in an embed; Updated `ColourCodes` to implement a `to_embed_colour()` function

--- src/runtime/event_handler.rs
+++ src/runtime/event_handler.rs
@@ -1,5 +1,7 @@
 // --== MODULE IMPORTS ==-- //
 
+    use std::fmt::Pointer;
+
     use crate::{
         runtime::{
             context_keys,
@@ -16,16 +18,12 @@
 
     // SERENITY
         use serenity::{
-            async_trait,
-            model::{
-                id::ChannelId,
-                gateway::Ready
-            },
-            client::{
+            all::{CreateEmbed, Timestamp}, async_trait, builder::CreateMessage, client::{
                 Context,
                 EventHandler
-            },
-            builder::CreateMessage,
+            }, model::{
+                gateway::Ready, id::ChannelId
+            }
         };
 // ==--
 
@@ -36,21 +34,54 @@ impl EventHandler for runtime_client::RuntimeClient {
     async fn ready( &self, ctx: Context, _ready: Ready) {
 
         // --== CREATE & SEND WAKEUP MESSAGE ==-- //
+        
+            // The startup process went swimmingly, let us not be shy and send a wakeup message. It
+            // will be a simple embed whose title is simply "Magician Online", and which contains a
+            // timestamp defining when Magician here came online
             
             // First of all, we need to read the channel id towards which, we will send the wakeup
-            // message
-            let wakeup_channel_id = *ctx.data.read().await.get::<context_keys::WakeupChannelIdKey>()
+            // message. It is read at startup from the config file and stored here in our Context
+             let wakeup_channel_id = *ctx.data.read().await.get::<context_keys::WakeupChannelIdKey>()
                 .expect("Wakeup channel id should be stored in Context");
 
-            // 
-
-            let wakeup_message = CreateMessage::new().content("Hello World!");
-            let wakeup_channel = ChannelId::from(wakeup_channel_id);
-            let _temp = wakeup_channel.send_message(&ctx.http, wakeup_message).await;
-
-            println!( "{}",
-                create_log_message("Bot Online!", ColourCode::Info)
+            // Next up, we need to define the colour and timestamp to be used by our embed, and
+            // additionally, the channel towards which we shall fire it
+            let ( embed_colour, embed_timestamp, wakeup_channel ) = (
+                ColourCode::Info.to_embed_colour(),
+                Timestamp::now(),
+                ChannelId::from(wakeup_channel_id)
             );
+
+            // Then we put all of those together into an embed builder
+            let wakeup_embed = CreateEmbed::new()
+                .title("Magician Online")
+                .colour(embed_colour)
+                .timestamp(embed_timestamp);
+
+            // And pack it into a message
+            let wakeup_message = CreateMessage::new()
+                .embed(wakeup_embed);
+
+            // And send it down the wire, reporting any errors should they occur
+            if let Err(why) = wakeup_channel.send_message(&ctx.http, wakeup_message).await {
+                println!( "{}", create_log_message(
+                    format!(
+                        "`{}ready{}`: Failed to send wakeup message: `{}{}{}`",
+                        ColourCode::Location,
+                        ColourCode::Reset,
+                        ColourCode::Info,
+                        why,
+                        ColourCode::Reset
+                    ),
+                    ColourCode::Error
+                ));
+            }
         // ==--
+
+        // Oh and also, lets tell the terminal that we are online
+        println!( "{}",
+            create_log_message("Bot Online!", ColourCode::Info)
+        );
     }
 }
+
--- src/utils/misc/colour_codes.rs
+++ src/utils/misc/colour_codes.rs
@@ -1,5 +1,5 @@
 use std::fmt::Display;
-
+use serenity::model::Colour;
 use termion::color;
 
 
@@ -14,15 +14,30 @@ pub enum ColourCode {
     Success,
     Location
 }
+impl ColourCode {
+    pub fn to_embed_colour(&self) -> Colour {
+        match self {
+            ColourCode::Reset  => Colour::from_rgb(0, 0, 0),
+            ColourCode::Info   => Colour::from_rgb(0, 255, 255),
+            ColourCode::Field  => Colour::from_rgb(0, 0, 255),
+            ColourCode::Error  => Colour::from_rgb(255, 0, 0),
+
+            ColourCode::Warning   => Colour::from_rgb(128, 128, 0),
+            ColourCode::Caution   => Colour::from_rgb(255, 255, 0),
+            ColourCode::Success   => Colour::from_rgb(0, 255, 0),
+            ColourCode::Location  => Colour::from_rgb(255, 0, 255)
+        }
+    }
+}
 
 impl Display for ColourCode {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
 
         match self {
-            ColourCode::Reset     =>  write!(  f, "{}", color::Reset.fg_str()        ),
-            ColourCode::Info      =>  write!(  f, "{}", color::LightCyan.fg_str()    ),
-            ColourCode::Field     =>  write!(  f, "{}", color::LightBlue.fg_str()    ),
-            ColourCode::Error     =>  write!(  f, "{}", color::LightRed.fg_str()     ),
+            ColourCode::Reset  =>  write!(  f, "{}", color::Reset.fg_str()      ),
+            ColourCode::Info   =>  write!(  f, "{}", color::LightCyan.fg_str()  ),
+            ColourCode::Field  =>  write!(  f, "{}", color::LightBlue.fg_str()  ),
+            ColourCode::Error  =>  write!(  f, "{}", color::LightRed.fg_str()   ),
             
             ColourCode::Warning   =>  write!(  f, "{}", color::Yellow.fg_str()       ),
             ColourCode::Caution   =>  write!(  f, "{}", color::LightYellow.fg_str()  ),
